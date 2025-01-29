f15a2e3d791c46305b7521994b779fdafc09e27a
ekuinox <depkey@me.com>
2025-01-29 23:30:28 +0900

    toml だけで良い

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,6 @@ const VERSION: &str = formatcp!(
 #[clap(version = VERSION)]
 pub struct Args {
     pub path: Option<PathBuf>,
-
-    #[clap(short, long, default_value = "toml")]
-    pub output_format: OutputFormat,
 }
 
 #[derive(ValueEnum, Default, PartialEq, Eq, Clone, Copy, Debug)]
@@ -27,10 +24,7 @@ pub enum OutputFormat {
 }
 
 fn main() {
-    let Args {
-        path,
-        output_format,
-    } = Args::parse();
+    let Args { path } = Args::parse();
 
     let paths = path
         .map(|path| Box::new([path].into_iter()) as Box<dyn Iterator<Item = PathBuf>>)
@@ -43,12 +37,8 @@ fn main() {
 
         let envs = parse_dotenv(&env_text).expect("Failed to parse .env");
 
-        let output = match output_format {
-            OutputFormat::Toml => toml::to_string(&envs).expect("Failed to serialize to TOML."),
-            OutputFormat::Json => {
-                serde_json::to_string_pretty(&envs).expect("Failed to serialize to JSON.")
-            }
-        };
+        let output = toml::to_string(&envs).expect("Failed to serialize to TOML.");
+
         println!("{output}");
 
         break;
