86340bcdbcc06dc06b39a5b0beba1bcad1a4a4f3
ekuinox <depkey@me.com>
2025-01-29 23:28:20 +0900

    パスの指定の無い場合、 .env, .envrc の順に読んでいって最初に見つけたものを使う

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,7 @@ const VERSION: &str = formatcp!(
 #[derive(Parser)]
 #[clap(version = VERSION)]
 pub struct Args {
-    #[clap(default_value = ".env")]
-    pub path: PathBuf,
+    pub path: Option<PathBuf>,
 
     #[clap(short, long, default_value = "toml")]
     pub output_format: OutputFormat,
@@ -33,14 +32,25 @@ fn main() {
         output_format,
     } = Args::parse();
 
-    let env_text = std::fs::read_to_string(&path).expect("Failed to open .env");
-    let envs = parse_dotenv(&env_text).expect("Failed to parse .env");
+    let paths = path
+        .map(|path| Box::new([path].into_iter()) as Box<dyn Iterator<Item = PathBuf>>)
+        .unwrap_or_else(|| Box::new([".env", ".envrc"].map(PathBuf::from).into_iter()));
 
-    let output = match output_format {
-        OutputFormat::Toml => toml::to_string(&envs).expect("Failed to serialize to TOML."),
-        OutputFormat::Json => {
-            serde_json::to_string_pretty(&envs).expect("Failed to serialize to JSON.")
-        }
-    };
-    println!("{output}");
+    for path in paths {
+        let Ok(env_text) = std::fs::read_to_string(&path) else {
+            continue;
+        };
+
+        let envs = parse_dotenv(&env_text).expect("Failed to parse .env");
+
+        let output = match output_format {
+            OutputFormat::Toml => toml::to_string(&envs).expect("Failed to serialize to TOML."),
+            OutputFormat::Json => {
+                serde_json::to_string_pretty(&envs).expect("Failed to serialize to JSON.")
+            }
+        };
+        println!("{output}");
+
+        break;
+    }
 }
